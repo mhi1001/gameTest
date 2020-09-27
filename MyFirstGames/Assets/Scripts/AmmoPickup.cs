c9615c398bc229606a9b135bce2d7d617ea06c8d
Pedro <pedr0374@edu.easj.dk>
2020-09-27 20:21:55 +0200

    Added my game project

---
+++ MyFirstGames/Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        GlobalAmmo.CurrentAmmo += 10;
+        this.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MyFirstGames/Assets/Scripts/GunFire.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour //This GunFire is the name of your C# file!!! 
+{
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            AudioSource gunsound = GetComponent<AudioSource>();
+            gunsound.Play();
+            GetComponent<Animation>().Play("GunShot");
+            GlobalAmmo.CurrentAmmo--;
+        }
+    }
+}
